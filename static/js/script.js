66e856da6325b5b734f08f2d6f108ad43db95f24
Hayley van Waas <hayleyavw@gmail.com>
2019-04-19 13:29:29 +0800

    Desktop version finishedish

--- static/js/script.js
+++ static/js/script.js
@@ -2,3 +2,11 @@
 // 	container: 'viewer',
 // 	panorama: 'path/to/panorama.jpg'
 // });
+
+$(document).on("scroll", function(){
+	if ($(document).scrollTop() > 100) {
+	    $("header").addClass("shrink");
+	} else {
+		$("header").removeClass("shrink");
+	}
+});
