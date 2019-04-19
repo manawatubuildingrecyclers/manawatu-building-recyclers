ec95496032c5047840f26868e0e56e17e29e9999
Hayley van Waas <hayleyavw@gmail.com>
2019-04-19 14:17:19 +0800

    improvements to interface

--- static/js/script.js
+++ static/js/script.js
@@ -2,11 +2,3 @@
 // 	container: 'viewer',
 // 	panorama: 'path/to/panorama.jpg'
 // });
-
-$(document).on("scroll", function(){
-	if ($(document).scrollTop() > 100) {
-	    $("header").addClass("shrink");
-	} else {
-		$("header").removeClass("shrink");
-	}
-});
